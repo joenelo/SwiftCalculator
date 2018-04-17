b61cc392b1cc1af57d1f73e3044701dfa68a52ff
Joe Nelson <joeandrewnelson@gmail.com>
2018-04-17 02:10:48 -0700

    second commit, UI Complete with Constraints. - Calculator Functional. Error handling in processes. can't turn off all buttons when an Error returned.

---
+++ SwiftCalculator/AppDelegate/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  SwiftCalculator
+//
+//  Created by joseph nelson on 2018-04-16.
+//  Copyright © 2018 joseph nelson. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+@IBDesignable
+class RoundButtons: UIButton {
+    @IBInspectable var roundButton:Bool = false {
+        didSet {
+            if roundButton {
+                layer.cornerRadius = frame.height/2
+                
+            }
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        if roundButton {
+            layer.cornerRadius = frame.height/2
+        }
+    }
+}
+
---
+++ SwiftCalculator/Calculate/CalculatorController.swift
@@ -0,0 +1,79 @@
+
+import UIKit
+
+class CalculatorController: UIViewController {
+    // create variables for Calculator use
+    var currentNumbers: Double = 0
+    var previousNumbers: Double = 0
+    var doingMathCalcs = false
+    var operation = 0
+    
+    
+    // create a clearButton function to clean the TextView
+    @IBAction func clearButton(_ sender: UIButton) {
+        answerTextView.text = ""
+    }
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    // Numbers function for all for the Numbered buttons
+    @IBAction func numbers(_ sender: UIButton) {
+        // if doing pressing the number buttons set their numbers to the tags -1  and make them Doubles
+        if doingMathCalcs == true {
+            answerTextView.text = String(sender.tag-1)
+            currentNumbers = Double(answerTextView.text!)!
+            doingMathCalcs = false
+            
+        } else {
+            answerTextView.text = answerTextView.text! + String(sender.tag-1)
+            currentNumbers = Double(answerTextView.text!)!
+        }
+    }
+    // create a function for the Math operator buttons
+    @IBAction func mathButtons(_ sender: UIButton) {
+        if answerTextView.text != "" && sender.tag != 15 {
+            // Save the first number from the TextView to a variable and set it to a double.
+            previousNumbers = Double(answerTextView.text!)!
+
+            if sender.tag == 11 { //-Addition
+                answerTextView.text = "+"
+                answerTextView.text = "" // Don't show the Math operator sign
+            }
+            if sender.tag == 12 { //-Substraction
+                answerTextView.text = "-"
+                answerTextView.text = ""
+            }
+            if sender.tag == 13 { //-Multiplication
+                answerTextView.text = "*"
+                answerTextView.text = ""
+            }
+            if sender.tag == 14 { //-Division
+                answerTextView.text = "/"
+                answerTextView.text = ""
+            }
+            // save the sender tags into a variable
+            operation = sender.tag
+            doingMathCalcs = true
+        }
+            // create an area to do simple math problems, previously stored numbers plus whats in the textview currently
+        else if sender.tag == 15 {
+            if operation == 11 {
+                answerTextView.text = String(previousNumbers + currentNumbers)
+            }
+            else if operation == 12 {
+                answerTextView.text = String(previousNumbers - currentNumbers)
+            }
+            else if operation == 13 {
+                answerTextView.text = String(previousNumbers * currentNumbers)
+            }
+            else if operation == 14 {
+                if currentNumbers > 0  {
+                    answerTextView.text = String(previousNumbers / currentNumbers)
+            } else {
+                answerTextView.text = String("Error")
+               
+            }
+    
+            }
+        }
+    }
+}
